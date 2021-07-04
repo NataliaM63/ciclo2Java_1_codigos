b01559245d25d0a42608d2a345698d262bf2e736
NataliaM63 <dorisnatalia212@gmail.com>
2021-07-04 16:07:27 -0500

    añadi una nueva linea

--- PROGRAMACION BASICA/Grupo 52/ejemplo1/src/App.java
+++ PROGRAMACION BASICA/Grupo 52/ejemplo1/src/App.java
@@ -10,30 +10,32 @@ public class App {
      */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Misiontic");
-        int variable = 8; 
-        System.out.println(variable);
-        boolean variable2 = true;
-        String variable3 = "joshua"; 
-        float variable4 = 1.2f;
-        double variable5 = 1.8;
-        System.out.println(variable5 );
-        char variable6 = 'N';   // UNA SOLA LETRA 
-        System.out.println(variable6);
-        byte variable7 = -128;
-        System.out.println(variable7);
-        short variable8 = 5;
-        int variable9 = 5;
-        long variable10 = 5;
-        int [] lista = {5,9,3,2};
-        System.out.println(lista[1]);
-        String [] lista2 = {"Hola", "como", "estas",};
-        var variable11 = "hola";
-        System.out.println(variable11);
-        variable11 = "hola";
-        String variable12 = "hola\t\"mundo\"";
-        System.out.println(variable12);
-        System.out.println("\u00F6");
+        // System.out.println("Misiontic");
+        // int variable = 8; 
+        // System.out.println(variable);
+        // boolean variable2 = true;
+        // String variable3 = "joshua"; 
+        // float variable4 = 1.2f;
+        // double variable5 = 1.8;
+        // System.out.println(variable5 );
+        // char variable6 = 'N';   // UNA SOLA LETRA 
+        // System.out.println(variable6);
+        // byte variable7 = -128;
+        // System.out.println(variable7);
+        // short variable8 = 5;
+        // int variable9 = 5;
+        // long variable10 = 5;
+        // int [] lista = {5,9,3,2};
+        // System.out.println(lista[1]);
+        // String [] lista2 = {"Hola", "como", "estas",};
+        // var variable11 = "hola";
+        // System.out.println(variable11);
+        // variable11 = "hola";
+        // String variable12 = "hola\t\"mundo\"";
+        // System.out.println(variable12);
+        // System.out.println("\u00F6");
+
+        System.out.println("Hello, a todos");
     }
 }
  
